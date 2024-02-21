3758e254d2bcd9339c7d836ff76658fcdc386503
Cristianps1988 <cristianps_1@hotmail.com>
2024-02-21 11:49:27 -0500

    initial structure

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="bg-teal-600 h-screen flex justify-center align-middle">
+      <p className="text-white text-4xl">Holis</p>
+    </div>
+  )
+}
+
+export default App
